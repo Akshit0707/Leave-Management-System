20d2746a897b85a82f8302d73a2d2fd77aa3f13e
Akshit Arora <akshitarora2022@gmail.com>
2026-01-24 00:49:10 +0530

    Fix Railway DB startup and migrations

--- LeaveManagement.API/Program.cs
+++ LeaveManagement.API/Program.cs
@@ -72,23 +72,23 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+    if (!string.IsNullOrEmpty(databaseUrl))
+    {
+        // Parse as URI and convert to connection string
+        // Convert Railway URL → Npgsql connection string
+        var uri = new Uri(databaseUrl);
+        var userInfo = uri.UserInfo.Split(':');
 
-    if (string.IsNullOrWhiteSpace(databaseUrl))
-        throw new Exception("DATABASE_URL is not configured");
+        var connectionString =
+            $"Host={uri.Host};" +
+            $"Port={uri.Port};" +
+            $"Database={uri.AbsolutePath.TrimStart('/')};" +
+            $"Username={userInfo[0]};" +
+            $"Password={userInfo[1]};" +
+            $"SSL Mode=Require;Trust Server Certificate=true";
 
-    // Convert Railway URL → Npgsql connection string
-    var uri = new Uri(databaseUrl);
-    var userInfo = uri.UserInfo.Split(':');
-
-    var connectionString =
-        $"Host={uri.Host};" +
-        $"Port={uri.Port};" +
-        $"Database={uri.AbsolutePath.TrimStart('/')};" +
-        $"Username={userInfo[0]};" +
-        $"Password={userInfo[1]};" +
-        $"SSL Mode=Require;Trust Server Certificate=true";
-
-    options.UseNpgsql(connectionString);
+        options.UseNpgsql(connectionString);
+    }
 });
 
 
