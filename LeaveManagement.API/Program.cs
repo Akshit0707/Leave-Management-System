2ad74bd1b5dc814b082d8e5d9cada860b5bc1f63
Akshit Arora <akshitarora2022@gmail.com>
2026-01-23 20:39:46 +0530

    Use public Railway backend URL

--- LeaveManagement.API/Program.cs
+++ LeaveManagement.API/Program.cs
@@ -106,6 +106,12 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 /* =======================
    PIPELINE
 ======================= */
