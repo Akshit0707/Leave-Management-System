767266fb0e9e12d4c7ed5b648994dc53d04016cd
Akshit Arora <akshitarora2022@gmail.com>
2026-01-25 17:07:10 +0530

    Admin Panel

--- LeaveManagement.API/Services/ILeaveService.cs
+++ LeaveManagement.API/Services/ILeaveService.cs
@@ -5,8 +5,8 @@ public interface ILeaveService
 {
     Task<LeaveRequestDto?> CreateAsync(int userId, CreateLeaveRequest request);
     Task<IEnumerable<LeaveRequestDto>> GetMyAsync(int userId);
-    Task<IEnumerable<LeaveRequestDto>> GetPendingAsync();
-    Task<IEnumerable<LeaveRequestDto>> GetAllAsync();
+    Task<IEnumerable<LeaveRequestDto>> GetAllForManagerAsync(int managerId);
+    Task<IEnumerable<LeaveRequestDto>> GetPendingForManagerAsync(int managerId);
     Task<bool> UpdateStatusAsync(int id, UpdateLeaveRequest request, int managerId);
     Task<LeaveSummaryDto> GetSummaryAsync(int userId, bool isManager);
     Task<bool> DeleteAsync(int id, int userId);
