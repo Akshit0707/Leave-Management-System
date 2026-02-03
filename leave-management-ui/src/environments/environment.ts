028d795bb97f10039903d10209de8e107060c056
Akshit Arora <akshitarora2022@gmail.com>
2026-02-03 20:07:04 +0530

    Netlify Deployment

--- leave-management-ui/src/environments/environment.ts
+++ leave-management-ui/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://leave-management-system.up.railway.app'
+
   apiUrl: 'https://leavemgmt-hqgybuaccpf6f6an.eastasia-01.azurewebsites.net'
 };
 
